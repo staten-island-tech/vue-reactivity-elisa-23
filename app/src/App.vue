cd83de432cd9c3b1bb7edb750cdebe3afd82b765
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-01-28 12:41:20 -0500

    sdfsadf

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,28 @@
-<template>
-  <div>
-
-  </div>
-</template>
-
 <script setup>
-
+import { RouterLink, RouterView } from 'vue-router'
+import './assets/main.css'
 </script>
 
-<style lang="scss" scoped>
+<template>
+  <header>
 
+    <div class="" id="body">
+      <div class="container mx-auto p-4">
+        <h1 class="text-3xl font-bold text-center mb-4">Hello Vite!</h1>
+        <button class="btn btn-primary">Click Me</button>
+    </div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+#body{
+  background-color: bisque;
+}
 </style>
