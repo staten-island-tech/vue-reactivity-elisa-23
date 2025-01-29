62d0ddb525ac7528b25d3c7e038ba67a0cb06595
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-01-28 20:52:40 -0500

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-
+  <div class="absolute w-[37rem] h-[46rem] rounded-[2rem] border-yellow-500 bg-white border-8 right-10 top-10 p-6">
+    <div class="self-center h-2 w-{20rem} bg-yellow-500 mt-2"></div>
   </div>
 </template>
 
 <script setup>
-
-
 
 </script>
 
