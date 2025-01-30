4b25f4215cf9f4c5757681a7d191c48cde8c243e
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-01-30 12:31:50 -0500

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,11 @@
 <template>
+  <div class="">
+    <button class="w-[5rem] h-[3rem] bg-yellow-950 rounded-md text-amber-100 ">Inventory</button>
+    <button class="w-[10rem] h-[6rem] bg-yellow-950 rounded-sm">Shopping Cart</button>
+  </div>
   <div class="absolute w-[37rem] h-[46rem] rounded-[2rem] border-yellow-500 bg-white border-8 right-10 top-10 p-6">
+    <div class="self-center h-2 w-{20rem} bg-yellow-500 mt-2"></div>
+    <div class="self-center h-14"></div>
     <div class="self-center h-2 w-{20rem} bg-yellow-500 mt-2"></div>
   </div>
 </template>
