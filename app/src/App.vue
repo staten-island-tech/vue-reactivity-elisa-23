6a0c2c80f97df25604bbf75e6bec0898c5151a35
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-01-30 16:11:14 -0500

    jkhlkjh

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="">
-    <button class="w-[5rem] h-[3rem] bg-yellow-950 rounded-md text-amber-100 ">Inventory</button>
-    <button class="w-[10rem] h-[6rem] bg-yellow-950 rounded-sm">Shopping Cart</button>
-  </div>
+  <button class="absolute w-[7rem] h-[3rem] bg-yellow-950 rounded-md text-amber-100 top-8 left-7 font-bold font-mono">
+    <RouterLink to="/" event>Inventory</RouterLink>
+  </button>
+  <button class="absolute w-[7rem] h-[3rem] bg-yellow-950 rounded-md text-amber-100 top-8 left-40 font-bold font-mono">Shopping Cart</button>
   <div class="absolute w-[37rem] h-[46rem] rounded-[2rem] border-yellow-500 bg-white border-8 right-10 top-10 p-6">
     <div class="self-center h-2 w-{20rem} bg-yellow-500 mt-2"></div>
     <div class="self-center h-14"></div>
@@ -11,6 +11,8 @@
 </template>
 
 <script setup>
+import { RouterLink } from 'vue-router';
+
 
 </script>
 
