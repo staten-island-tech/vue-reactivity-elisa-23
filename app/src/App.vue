4fc247bb8fb99e6e79305e34b8887f28d22f8dc4
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-01-30 16:14:03 -0500

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,7 +2,9 @@
   <button class="absolute w-[7rem] h-[3rem] bg-yellow-950 rounded-md text-amber-100 top-8 left-7 font-bold font-mono">
     <RouterLink to="/" event>Inventory</RouterLink>
   </button>
-  <button class="absolute w-[7rem] h-[3rem] bg-yellow-950 rounded-md text-amber-100 top-8 left-40 font-bold font-mono">Shopping Cart</button>
+  <button class="absolute w-[7rem] h-[3rem] bg-yellow-950 rounded-md text-amber-100 top-8 left-40 font-bold font-mono">
+    <RouterLink to="/" event>Shopping Cart</RouterLink>
+  </button>
   <div class="absolute w-[37rem] h-[46rem] rounded-[2rem] border-yellow-500 bg-white border-8 right-10 top-10 p-6">
     <div class="self-center h-2 w-{20rem} bg-yellow-500 mt-2"></div>
     <div class="self-center h-14"></div>
