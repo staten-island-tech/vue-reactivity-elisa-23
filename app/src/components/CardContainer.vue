1c888752b2c48879ed17f068ccae3dca0df86f3b
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-01-31 12:07:05 -0500

    werwe

--- app/src/components/CardContainer.vue
+++ app/src/components/CardContainer.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="absolute right-[5%] top-45 grid col-3 row-3 bg-black w-[33rem] h-[30rem]">
-      hi
+    <div class="absolute right-[4.25%] top-45 grid col-3 row-3 bg-black w-[33rem] h-[30rem]">
+      
     </div>
-    <button class="absolute right-[15%] bottom-[12.5%]">></button>
+    <button class="absolute right-[15%] bottom-[18%] rounded-full hover:bg-amber-200 active:bg-amber-400 h-8 w-8 font-bold font-mono text-yellow-700">></button>
+    <button class="absolute right-[21.75%] bottom-[18%] rounded-full hover:bg-amber-200 active:bg-amber-400 h-8 w-8 font-bold font-mono text-yellow-700"><</button>
   </template>
   
   <script setup>
