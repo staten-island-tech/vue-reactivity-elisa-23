ab197da1f96b88e8272bb9037d62effe74b9fbfa
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-01-31 12:29:45 -0500

    sdfsd

--- app/src/App.vue
+++ app/src/App.vue
@@ -11,7 +11,9 @@
     <p>Cart</p>
   </button>
   </RouterLink>
-  
+  <div class="bg-white absolute top-[5%] right-[38%] h-[15rem] w-[7.5rem] rounded-md shadow-md">
+    
+  </div>
   <div class="absolute w-[37rem] h-[46rem] rounded-[2rem] border-yellow-500 bg-white border-8 right-10 top-10 p-6">
     <div class="self-center h-2 w-{20rem} bg-yellow-500 mt-2"></div>
     <div class="self-center h-14">
