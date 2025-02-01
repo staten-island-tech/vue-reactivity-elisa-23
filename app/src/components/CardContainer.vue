517eec5470bacd8d90f09ad5fa974ec31e37e871
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-01-31 19:05:26 -0500

    dfssd

--- app/src/components/CardContainer.vue
+++ app/src/components/CardContainer.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="absolute w-[37%] h-[90%] right-10 top-10 p-6">
-    <div class="absolute grid col-3 row-3 w-[88%] h-[30rem] top-[18%] left-[6%] bg-black">
-      <div class="w-[27%] h-[27%] bg-yellow-500 m-3 mr-5"></div>
-      <div class="w-[27%] h-[27%] bg-yellow-500 m-3 mr-5"></div>
+    <div class="absolute grid grid-cols-3 grid-rows-3 w-[88%] h-[30rem] top-[18%] left-[6%] place-items-center">
+      <Card v-for="card in cards" :key="card.name" :card="card"></Card>
     </div>
     <button class="absolute bottom-[10%] left-[35%] rounded-full hover:bg-amber-200 active:bg-amber-400 h-8 w-8 font-bold font-mono text-yellow-700"><</button>
     <button class="absolute bottom-[10%] right-[35%] rounded-full hover:bg-amber-200 active:bg-amber-400 h-8 w-8 font-bold font-mono text-yellow-700">></button>
   </div>
-  
-  </template>
-  
-  <script setup>
-  
-  </script>
-  
-  <style scoped>
-  
-  </style>
+</template>
+
+<script setup>
+import Card from './CardProps.vue';
+const cards = [{img: "url", description: "hi", name: "table" }, {img: "url", description: "hi", name: "table" }, {img: "url", description: "hi", name: "table" }, {img: "url", description: "hi", name: "table" }];
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/components/CardProps.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-yellow-500 h-[80%] w-[80%]">
+        <button class="h-[100%] w-[100%] items-center hover:bg-yellow-200 active:bg-yellow-950">
+            <img class="h-[80%] w-[80%]" :src="card.img" :alt="card.description">
+        </button>
+    </div>
+</template>
+
+<script setup>
+defineProps ({
+    card: Object
+})
+</script>
+
+<style scoped>
+
+</style>
