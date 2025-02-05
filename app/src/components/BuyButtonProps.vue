dc7f2470766cafaf841044ed1b52af2af97eaa22
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-02-05 18:17:28 -0500

    sadsfdghfjgkhl

--- app/src/components/BuyButtonProps.vue
+++ app/src/components/BuyButtonProps.vue
@@ -4,20 +4,58 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { cart } from '@/variables.js';
+import { ref, watch, onMounted } from 'vue';
+import { cart, selected } from '@/variables.js';
 import { cards } from '@/plush.js';
 
-let message = ref("Add to Cart");
-let buttonClass = ref("bg-white w-[30rem] h-[7rem] absolute bottom-0 font-bold text-amber-950 text-[3rem] hover:bg-yellow-200 active:bg-yellow-500 active:text-white");
+let message = ref("");
+let buttonClass = ref("");
+
+onMounted (() => {
+    resetButton();
+})
+
+watch (selected, () => {
+    resetButton();
+});
+
+function resetButton () {   
+    cards.forEach((card) => {
+        if (card.id === selected.value) {
+            if (card in cart.items) {
+                message.value = "Added to Cart";
+                buttonClass.value = "bg-yellow-500 w-[30rem] h-[7rem] absolute bottom-0 font-bold text-[3rem] text-white";
+            } else {
+                message.value = "Add to Cart";
+                buttonClass.value = "bg-white w-[30rem] h-[7rem] absolute bottom-0 font-bold text-amber-950 text-[3rem] hover:bg-yellow-200 active:bg-yellow-500 active:text-white";
+            }
+        }
+    });
+}
 function toCart() {
     if (message.value === "Add to Cart"){
         message.value = "Added to Cart";
         buttonClass.value = "bg-yellow-500 w-[30rem] h-[7rem] absolute bottom-0 font-bold text-[3rem] text-white";
-        cart.value.push()
+        cards.forEach((card) => {
+            if (card.id === selected.value) {
+                    card.stock --;
+                    cart.items.push(card);
+                    console.log(cart.items);
+                    console.log(card);
+            }
+        });
     }else {
         message.value = "Add to Cart";
         buttonClass.value = "bg-white w-[30rem] h-[7rem] absolute bottom-0 font-bold text-amber-950 text-[3rem] hover:bg-yellow-200 active:bg-yellow-500 active:text-white";
+        cards.forEach((card) => {
+            if (card.id === selected.value) {
+                    card.stock ++;
+                    cart.items.splice(cart.items.indexOf(card), 1)
+                    cart.items = newCart;
+                    console.log(cart.items);
+                    console.log(card);
+            }
+        });
     }
 }
 
--- app/src/variables.js
+++ app/src/variables.js
@@ -4,6 +4,6 @@ import { ref, reactive } from 'vue';
 let selected = ref("");
 let selectedObj = reactive({ img: null, description: "", name: "", id: "", price: null });
 
-let cart = reactive([]);
+let cart = reactive({ items: [] });
 
 export { selected, selectedObj, cart }
