d968ac9ff9f4316c69c6a5ee4750cd63fd53f608
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-02-06 18:17:43 -0500

    asdfghjkl

--- app/src/components/ReceiptContainer.vue
+++ app/src/components/ReceiptContainer.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
-
+    <div v-if="cart" class="absolute w-[37rem] h-[49rem] right-10 top-10 p-6 flex items-center justify-center" >
+        <div class="bg-black w-[90%] h-[50%] text-center font-stretch-expanded text-yellow-600 p-8 receipt overflow-y-scroll scroll-smooth">
+            
+        </div>
     </div>
 </template>
 
 <script setup>
-import { cart } from '@/variables.png';
+import { cart } from '@/variables.js';
 import { onMounted, ref, watch } from 'vue';
 
 let total = ref(0);
@@ -14,24 +16,29 @@ let tax = ref(0);
 
 onMounted(() => {
     priceCalc();
-})
+});
 
-watch(cart, () => {
+watch(cart.items, () => {
     priceCalc();
-})
+});
 
 function priceCalc() {
-    cart.forEach((item) => {
+    total.value = 0;
+    cart.items.forEach((item) => {
         total.value += item.price;
     });
-    subTotal.value = total.value;
-    tax.value = total.value * 0.08875;
-    total.value = total.value * 1.08875;
-    console.log(total.value);
+    subTotal.value = Math.round(total.value * 100) / 100;
+    total.value = Math.round((total.value * 1.08875) * 100) / 100;
+    tax.value = total.value - subTotal.value;
+    console.log(tax.value);
 }
 
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Hubballi&family=Julius+Sans+One&display=swap');
 
+.receipt {
+    font-family: "Julius Sans One", serif;
+}
 </style>
