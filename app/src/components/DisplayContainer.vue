5e03f55fc30bae8c9b762aca496b7fb00fe94d64
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-02-05 19:20:14 -0500

    Update DisplayContainer.vue

--- app/src/components/DisplayContainer.vue
+++ app/src/components/DisplayContainer.vue
@@ -2,6 +2,7 @@
     <div class="h-[44rem] w-[44rem] absolute top-23 left-40 flex justify-center">
         <img v-if="selectedObj.img" :src="img" :alt="description" class="h-[37rem]">
         <BuyButton v-if="selectedObj.img" />
+        <p v-if="selectedObj.img" class="absolute bottom-[-3rem] font-bold text-yellow-950 text-[1.5rem]">price: {{ price }}</p>
     </div>
 </template>
 
@@ -12,6 +13,7 @@ import { computed } from 'vue';
 
 const img = computed(() => selectedObj.img || '');
 const description = computed(() => selectedObj.description || '');
+const price = computed(() => selectedObj.price || '');
 
 </script>
 
