b0dcbed4f7b896bd3315272ceff046fdd287834c
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-02-06 12:31:02 -0500

    sdfa

--- app/src/components/CardContainer.vue
+++ app/src/components/CardContainer.vue
@@ -1,17 +1,58 @@
 <template>
   <div class="absolute w-[37rem] h-[49rem] right-10 top-10 p-6">
     <div class="absolute grid grid-cols-3 grid-rows-3 w-[88%] h-[30rem] top-[18%] left-[6%] place-items-center">
-      <Card v-for="card in cards" :key="card.name" :card="card"></Card>
+      <Card v-for="card in displayedCards" :key="card.name" :card="card"></Card>
     </div>
-    <button class="absolute bottom-[10%] left-[35%] rounded-full hover:bg-amber-200 active:bg-amber-400 h-8 w-8 font-bold font-mono text-yellow-700" @click=""><</button>
-    <button class="absolute bottom-[10%] right-[35%] rounded-full hover:bg-amber-200 active:bg-amber-400 h-8 w-8 font-bold font-mono text-yellow-700" @click="">></button>
+    <button class="absolute bottom-[10%] left-[35%] rounded-full hover:bg-amber-200 active:bg-amber-400 h-8 w-8 font-bold font-mono text-yellow-700" @click="changePage(back)"><</button>
+    <button class="absolute bottom-[10%] right-[35%] rounded-full hover:bg-amber-200 active:bg-amber-400 h-8 w-8 font-bold font-mono text-yellow-700" @click="changePage(forward)">></button>
   </div>
   
 </template>
 
 <script setup>
+import { onMounted, reactive, ref, watch } from 'vue';
 import Card from './CardProps.vue';
 import { cards } from '@/plush.js';
+
+const back = "<";
+const forward = ">";
+
+let displayedCards = reactive([]);
+let page = ref(1);
+onMounted(() => {
+  cards.forEach((card) => {
+    if (displayedCards.length < 9) {
+      displayedCards.push(card);
+    }
+  });
+  console.log(displayedCards);
+})
+
+function changePage(direction) {
+  displayedCards.splice(0, displayedCards.length);
+  if (direction === "<") {
+    if (page.value === 1) {
+      alert("Cannot turn back any further...");
+    } else {
+      page.value --;
+      cards.forEach((card) => {
+        if (displayedCards.length < 9) {
+          displayedCards.push(card);
+        }
+      });
+    }
+  } else {
+    if (page.value === 2) {
+      alert("Cannot go forward any further...");
+    } else {
+      page.value ++;
+      cards.forEach((card) => {
+        displayedCards.push(card);
+      });
+    }
+  }
+  console.log(page, displayedCards);
+}
 
 </script>
 
--- app/src/plush.js
+++ app/src/plush.js
@@ -2,11 +2,18 @@ let cards = [{ img: "plushies/white-rilakkuma.png", description: "a white rilakk
 { img: "plushies/pink-bunny.png", description: "a pink bunny plushie", name: "Pink Bunny", id: "pink-bunny", price: 19, stock: 1 },
 { img: "plushies/white-bunny.png", description: "a white bunny plushie", name: "White Bunny", id: "white-bunny", price: 17, stock: 1 },
 { img: "plushies/strawberry-my-melody.png", description: "a strawberry my melody plushie", name: "Strawberry My Melody", id: "strawberry-my-melody", price: 55, stock: 1 },
-{ img: "plushies/axolotl.png", description: "a axolotl plushie", name: "Axolotl", id: "axolotl", price: 10, stock: 1 },
+{ img: "plushies/axolotl.png", description: "an axolotl plushie", name: "Axolotl", id: "axolotl", price: 10, stock: 1 },
 { img: "plushies/chipmunk-penguin.png", description: "a chipmunk penguin plushie", name: "Chipmunk Penguin", id: "chipmunk-penguin", price: 15, stock: 1 },
 { img: "plushies/espeon.png", description: "an espeon plushie", name: "Espeon", id: "espeon", price: 35, stock: 1 },
 { img: "plushies/halloween-hello-kitty.png", description: "a halloween hello kitty plushie", name: "Halloween Hellow Kitty", id: "halloween-hello-kitty", price: 75, stock: 1 },
-{ img: "plushies/jigglypuff.png", description: "a jigglypuff plushie", name: "Jigglypuff", id: "jigglypuff", price: 25, stock: 1 }
+{ img: "plushies/jigglypuff.png", description: "a jigglypuff plushie", name: "Jigglypuff", id: "jigglypuff", price: 25, stock: 1 },
+{ img: "plushies/mouse.png", description: "a mouse plushie", name: "Mouse", id: "mouse", price: 15, stock: 1 },
+{ img: "plushies/pikachu.png", description: "a pikachu plushie", name: "Pikachu", id: "pikachu", price: 30, stock: 1 },
+{ img: "plushies/pink-dragon.png", description: "a pink dragon plushie", name: "Pink Dragon", id: "pink-dragon", price: 10, stock: 1 },
+{ img: "plushies/red-fox.png", description: "a red fox plushie", name: "Red Fox", id: "red-fox", price: 8, stock: 1 },
+{ img: "plushies/shark.png", description: "a shark plushie", name: "Shark", id: "shark", price: 10, stock: 1 },
+{ img: "plushies/star-rilakkuma.png", description: "a star rilakkuma plushie", name: "Star Rilakkuma", id: "star-rilakkuma", price: 45, stock: 1 },
+{ img: "plushies/stitch.png", description: "an stitch plushie", name: "Stitch", id: "stitch", price: 35, stock: 1 }
 ];
 
 export { cards };
