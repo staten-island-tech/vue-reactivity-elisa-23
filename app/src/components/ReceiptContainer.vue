ea2c4d948730188e9b287c8c7c1000b9b25552b2
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-02-06 17:20:06 -0500

    j;

---
+++ app/src/components/ReceiptContainer.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { cart } from '@/variables.png';
+import { onMounted, ref, watch } from 'vue';
+
+let total = ref(0);
+let subTotal = ref(0);
+let tax = ref(0);
+
+onMounted(() => {
+    priceCalc();
+})
+
+watch(cart, () => {
+    priceCalc();
+})
+
+function priceCalc() {
+    cart.forEach((item) => {
+        total.value += item.price;
+    });
+    subTotal.value = total.value;
+    tax.value = total.value * 0.08875;
+    total.value = total.value * 1.08875;
+    console.log(total.value);
+}
+
+</script>
+
+<style scoped>
+
+</style>
