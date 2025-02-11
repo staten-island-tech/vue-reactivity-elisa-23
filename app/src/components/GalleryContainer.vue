72dd3b7b07c5738319457790856b61251c0afa59
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-02-11 12:32:32 -0500

    dsfs

---
+++ app/src/components/GalleryContainer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="h-[70%] w-[70%] absolute top-[15%] left-[15%]">
+        <img v-for="image in cartImages" :src="image" alt="plush image" class="object-cover">
+    </div>
+</template>
+
+<script setup>
+import { cart } from '@/variables.js';
+import { onMounted, reactive } from 'vue';
+
+let cartImages = reactive([]);
+
+onMounted (() => {
+    refreshImg();
+})
+
+watch(cart.images, () => {
+    refreshImg();
+})
+
+function refreshImg() {
+    cartImages.splice(0, cartImages.length);
+    cart.items.forEach((item) => {
+        cartImages.push(item.img);
+    });
+}
+</script>
+
+<style scoped>
+
+</style>
--- app/src/views/CartView.vue
+++ app/src/views/CartView.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+    <GalleryContainer />
     <ReceiptContainer />
   </div>
 </template>
 
 <script setup>
 import ReceiptContainer from '@/components/ReceiptContainer.vue';
+import GalleryContainer from '@/components/GalleryContainer.vue';
 </script>
 
 <style scoped>
