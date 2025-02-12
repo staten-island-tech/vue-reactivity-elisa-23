1972893c17f24ee9f048d82775326f18921c9dfc
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-02-12 12:32:21 -0500

    dsf

--- app/src/components/GalleryContainer.vue
+++ app/src/components/GalleryContainer.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="h-[70%] w-[70%] absolute top-[15%] left-[15%]">
-        <img v-for="image in cartImages" :src="image" alt="plush image" class="object-cover">
+    <div v-if="cart.items" class="h-[55%] w-[30%] absolute top-[25%] left-[15%] bg-[url('/claw-machine.png')] bg-cover flex justify-center items-center shadow-purple-300 shadow-2xl">
+        <div class="h-[40%] w-[40%] overflow-y-scroll flex flex-col justify-center items-center">
+            <img v-for="image in cartImages" :src="image" alt="plush image" class="h-[90%]">
+        </div>
     </div>
 </template>
 
 <script setup>
 import { cart } from '@/variables.js';
-import { onMounted, reactive } from 'vue';
+import { onMounted, watch, reactive } from 'vue';
 
 let cartImages = reactive([]);
 
@@ -14,7 +16,7 @@ onMounted (() => {
     refreshImg();
 })
 
-watch(cart.images, () => {
+watch(cart.items, () => {
     refreshImg();
 })
 
