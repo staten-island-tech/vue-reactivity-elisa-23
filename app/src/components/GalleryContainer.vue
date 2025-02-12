703275c4f6b19151d546fff5193ee8c8950378a1
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-02-12 16:13:17 -0500

    dfghjk

--- app/src/components/GalleryContainer.vue
+++ app/src/components/GalleryContainer.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="cart.items" class="h-[55%] w-[30%] absolute top-[25%] left-[15%] bg-[url('/claw-machine.png')] bg-cover flex justify-center items-center shadow-purple-300 shadow-2xl">
-        <div class="h-[40%] w-[40%] overflow-y-scroll flex flex-col justify-center items-center">
-            <img v-for="image in cartImages" :src="image" alt="plush image" class="h-[90%]">
+        <div class="h-[40%] w-[40%] overflow-y-scroll flex flex-col items-center snap-y snap-proximity">
+            <img v-for="image in cartImages" :src="image" alt="plush image" class="h-[90%] m-1 snap-center">
         </div>
     </div>
 </template>
